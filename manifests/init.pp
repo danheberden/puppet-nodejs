e9c75c6c699f5dba7398d7433f35c81316ee75c8
Dan Heberden <danheberden@gmail.com>
2012-03-28 13:12:07 -0400

    Add build-essential package as a dependency

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,12 +6,15 @@ class nodejs ( $version, $logoutput = 'on_failure' ) {
   package { 'libssl-dev':
     ensure => present,
   }
+  package { 'build-essential':
+    ensure => present,
+  }
 
   # use nave, yo
   exec { 'nave' :
     command     => "bash -c \"\$(curl -s 'https://raw.github.com/isaacs/nave/master/nave.sh') usemain $version \"",
     path        => [ "/usr/local/bin", "/bin" , "/usr/bin" ],
-    require     => Package[ 'curl' ],
+    require     => [ Package[ 'curl' ], Package[ 'libssl-dev' ], Package[ 'build-essential' ] ],
     environment => [ 'HOME=""', 'PREFIX=/usr/local/lib/node' ],
     logoutput   => $logoutput,
   }
